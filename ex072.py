9b26b95a2edc217f5c7913394eea7fc0eaa5bdfc
Lucas Balbino S. Silva <lucas.silva844@fatec.sp.gov.br>
2021-05-20 00:02:16 -0300

    python exercises

---
+++ ex072.py
@@ -0,0 +1,7 @@
+numeros = ('zero', 'um', 'dois', 'três', 'quatro', 'cinco', 'seis', 'sete', 'oito', 'nove', 'dez', 'onze', 'doze',
+'treze', 'quatorze', 'quinze', 'dezesseis', 'dezessete', 'dezoito', 'dezenove', 'vinte')
+
+numusuario = int(input('Digite um número: '))
+while numusuario > 20 or numusuario < 0:
+    numusuario = int(input('Erro! digite um número: '))
+print('Você digitou o número \033[1;4;35m%s' % numeros[numusuario])
