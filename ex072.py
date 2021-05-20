9f9a407cbb0b15d34bf9ed5b5c5ed9753f6c8a95
Lucas Balbino S. Silva <lucas.silva844@fatec.sp.gov.br>
2021-05-20 09:15:27 -0300

    python exercises

--- ex072.py
+++ ex072.py
@@ -3,5 +3,5 @@ numeros = ('zero', 'um', 'dois', 'três', 'quatro', 'cinco', 'seis', 'sete', 'oi
 
 numusuario = int(input('Digite um número: '))
 while numusuario > 20 or numusuario < 0:
-    numusuario = int(input('Erro! digite um número: '))
+    numusuario = int(input('Erro! digite um número entre 0 e 20: '))
 print('Você digitou o número \033[1;4;35m%s' % numeros[numusuario])
---
+++ ex073.py
@@ -0,0 +1,15 @@
+campeonato = ('Flamengo', 'Internacional', 'Atlético-MG', 'São Paulo', 'Fluminense', 'Grêmio', 'Palmeiras', 'Santos', 'Athletico-PR', 'Bragantino', 'Ceará SC', 'Corinthans', 'Atlético-GO', 'Bahia', 'Sport Recife', 'Fortaleza', 'Vasco da Gama', 'Goiás', 'Coritiba', 'Botafogo')
+
+print('\033[1;4mPRIMEIROS CINCO COLOCADOS\033[m:')
+print(campeonato[0:5])
+print()
+
+print('\033[1;4mÚLTIMOS QUATRO COLOCADOS\033[m:')
+print(campeonato[16:21])
+print()
+
+print('\033[1;4mOS COLOCADOS EM ORDEM ALFABÉTICA\033[m:')
+print(sorted(campeonato))
+
+print('\033[1;4mEM QUAL ÍNDICE SE ENCONTRA O BOTAFOGO?\033[m:')
+print(campeonato.index('Botafogo'))
