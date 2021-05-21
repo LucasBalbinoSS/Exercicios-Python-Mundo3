bb47328334e4a5818d60e56bbaf9cd97b6ee785d
Lucas Balbino S. Silva <lucas.silva844@fatec.sp.gov.br>
2021-05-21 12:08:16 -0300

    python exercises

--- ex074.py
+++ ex074.py
@@ -1,7 +1,11 @@
 from random import randint as rdt
 
 contador = 0
-numeros = (rdt(0, 10), rdt(0, 10), rdt(0, 10), rdt(0, 10), rdt(0, 10))
+numeros = (rdt(0, 10),
+           rdt(0, 10),
+           rdt(0, 10),
+           rdt(0, 10),
+           rdt(0, 10))
 
 print(f'Os números sorteados foram: {numeros}')
 print(f'O maior número sorteado foi o número {max(numeros)}')
