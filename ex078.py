e2d15638ef62d6b57cf02f9cac8109760ba1af5e
Lucas Balbino S. Silva <lucas.silva844@fatec.sp.gov.br>
2021-05-22 14:01:19 -0300

    python exercises

---
+++ ex078.py
@@ -0,0 +1,10 @@
+numeros = list()
+
+for cont in range(1, 6):
+    numeros.append(int(input(f'Digite o {cont} valor: ')))
+
+print()
+print(f'O \033[1;4mmaior\033[m valor digitado foi {max(numeros)}', end='')
+print(f' que está na {numeros.index(max(numeros)) + 1}° posição')
+print(f'O \033[1;4mmenor\033[m valor digitado foi {min(numeros)}', end='')
+print(f' e se encontra na {numeros.index(min(numeros)) + 1}° posição')
