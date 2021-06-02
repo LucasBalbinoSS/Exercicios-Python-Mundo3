4fdd8e252842e3bea8d903ffc4a22ed129b4dab5
Lucas <82191848+LucasBalbinoSS@users.noreply.github.com>
2021-06-02 10:35:04 -0300

    Adicionando o segundo valor á lista.

--- ex080.py
+++ ex080.py
@@ -6,4 +6,11 @@ for cont in range(0, 5):
         lista.append(num)
         print('\033[35;1mPrimeiro valor dicionado à lista!\033[m')
 
+    elif len(lista) == 1 and num >= lista[0]:
+        lista.append(num)
+        print('\033[35;1mValor adicionado na segunda posição!\033[m')
+    else:
+        lista.insert(0, num)
+        print('\033[35;1mValor adicionado na primeira posição da lista!\033[m')
+        print(lista)
 print(lista)
