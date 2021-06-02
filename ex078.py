d491d3f4331ef8832b9d9355d5af19bf78cb3c35
Lucas <82191848+LucasBalbinoSS@users.noreply.github.com>
2021-06-02 18:35:31 -0300

    Melhorando o exercício.

--- ex078.py
+++ ex078.py
@@ -1,10 +1,30 @@
 numeros = list()
+maior = 0
+menor = 0
 
-for cont in range(1, 6):
+for cont in range(0, 5):
     numeros.append(int(input(f'Digite o {cont}° valor: ')))
+
+    if cont == 0:
+        maior = menor = numeros[cont]
+
+    elif numeros[cont] > maior:
+        maior = numeros[cont]
+
+    elif numeros[cont] < menor:
+        menor = numeros[cont]
 
 print()
 print(f'O \033[1;4mmaior\033[m valor digitado foi {max(numeros)}', end='')
-print(f' que está na {numeros.index(max(numeros)) + 1}° posição')
-print(f'O \033[1;4mmenor\033[m valor digitado foi {min(numeros)}', end='')
-print(f' e se encontra na {numeros.index(min(numeros)) + 1}° posição')
+print(f' que está nas posições ', end='')
+
+for i, v in enumerate(numeros):
+    if v == maior:
+        print(f'{i}... ', end='')
+
+print(f'\nO \033[1;4mmenor\033[m valor digitado foi {min(numeros)}', end='')
+
+for i, v in enumerate(numeros):
+    if v == menor:
+        print(f'{i}... ', end='')
+print(f'\n e se encontra na {numeros.index(min(numeros)) + 1}° posição')
