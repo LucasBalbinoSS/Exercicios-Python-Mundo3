44c5dd7e0a48d9866779ca4e2310582e88272392
Lucas <82191848+LucasBalbinoSS@users.noreply.github.com>
2021-06-02 10:23:44 -0300

    Adicionando primeiro valor à lista.

--- ex080.py
+++ ex080.py
@@ -2,10 +2,8 @@ lista = list()
 
 for cont in range(0, 5):
     num = int(input('Digite um número: '))
-    if len(lista) <= 2:
+    if len(lista) == 0:
         lista.append(num)
-        print('Adicionado na lista!')
-    elif len(lista) > 2:
-        pass
+        print('\033[35;1mPrimeiro valor dicionado à lista!\033[m')
 
 print(lista)
