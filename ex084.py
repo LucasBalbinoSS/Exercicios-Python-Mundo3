d28eda774f8b2b7dc737d4ce19b90a904b90c551
Lucas <82191848+LucasBalbinoSS@users.noreply.github.com>
2021-06-07 11:47:24 -0300

    python exercises.

---
+++ ex084.py
@@ -0,0 +1,10 @@
+database = list()
+nome = list()
+peso = list()
+contador = 0
+
+while True:
+    nome.append(str(input('Digite seu nome: ')).strip().title())
+    peso.append(int(input('Digite seu peso: ')))
+    database.append(nome[:])
+    database.append(peso[:])
