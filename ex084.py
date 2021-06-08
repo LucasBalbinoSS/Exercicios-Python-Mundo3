990f875ed7699939c9b8723d1d2637e5cbbcd57c
Lucas <82191848+LucasBalbinoSS@users.noreply.github.com>
2021-06-08 10:29:47 -0300

    adicionando o loop while.

--- ex084.py
+++ ex084.py
@@ -1,9 +1,16 @@
 listaDados = list()
 pessoas = list()
 
-for p in range(0, 5):
+while True:
     pessoas.append(str(input('Digite seu nome: ')).strip().title())
-    pessoas.append(int(input('Digite seu peso: Kg')))
+    pessoas.append(int(input('Digite seu peso (em Kg): ')))
+    print()
 
     listaDados.append(pessoas[:])
     pessoas.clear()
+
+    resp = ' '
+    while resp not in 'SN':
+        resp = str(input('Deseja continuar? [S/N]: ')).strip().upper()[0]
+    if resp == 'N':
+        break
