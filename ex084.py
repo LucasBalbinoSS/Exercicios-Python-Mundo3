2d18f769f97bad3b64af59e18beb6926e5ae7b95
Lucas Balbino <lucas.silva844@fatec.sp.gov.br>
2021-06-08 12:47:01 -0300

    saída de dados em andamento.

--- ex084.py
+++ ex084.py
@@ -12,5 +12,16 @@ while True:
     resp = ' '
     while resp not in 'SN':
         resp = str(input('Deseja continuar? [S/N]: ')).strip().upper()[0]
+        print()
     if resp == 'N':
         break
+
+print('=-' * 30)
+print(' ' * 15, '\033[34;1;4mANÁLISE DE DADOS\033[m')
+print()
+
+print(listaDados)
+if len(listaDados) == 1:
+    print(f'{len(listaDados)} pessoa foi cadastrada.')
+else:
+    print(f'{len(listaDados)} pessoas foram cadastradas.')
